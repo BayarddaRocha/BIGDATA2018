0968c15d243772148cdb5726bae4c047a6843833
BayarddaRocha <bayard.rocha@ufabc.edu.br>
2018-03-31 00:23:47 -0300

    novos exercs resolvidos

---
+++ Atividade_03/Exercicio-8.hs
@@ -0,0 +1,29 @@
+import Data.List
+import Data.Ord
+import qualified Data.Array as Array
+
+nextCollatz :: Integer -> Integer
+nextCollatz x
+  | even x    = x `div` 2
+  | otherwise = 3*x + 1
+     
+memoLenCollatz = lengths
+  where
+    lengths = Array.listArray (1, 1000000) [memoLen x | x <- [1..1000000]]
+    memoLen 1 = 1 
+    memoLen 2 = 2
+    memoLen 3 = 8
+    memoLen n 
+      | next <= 1000000 = 1 + lengths Array.! next
+      | otherwise       = 1 + memoLen next
+      where next = nextCollatz n
+    
+numeros = [1..1000000]    
+
+lengths = take 1000000
+        $ Array.assocs memoLenCollatz   -- retorna lista de tuplas (indice, valor)
+
+maior = fst $ maximumBy (comparing snd) lengths  -- retorna o maior elemento da lista comparado pelo segundo elemento da array
+
+main = do 
+  print maior
